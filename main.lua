dcccccc7115a5ab4db36de00df2de5b52b6a77e6
James Anderson <145408914+JamesAnderson30@users.noreply.github.com>
2025-03-18 10:56:33 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,34 @@
 
-function sayHello()
-    print("Hello World!")
+
+function love.load()
+  
+end
+
+x = 100
+y = 100
+moveSpeed = 50
+
+function love.update(dt)
+  if love.keyboard.isDown("w") then
+    y = y - moveSpeed * dt
+  end
+  
+  if love.keyboard.isDown("s") then
+    y = y + moveSpeed * dt
+  end
+  
+  if love.keyboard.isDown("d") then
+    x = x + moveSpeed * dt
+  end
+  
+  if love.keyboard.isDown("a") then
+    x = x - moveSpeed *dt
+  end
 end
 
 function love.draw()
-    love.graphics.print("Hello World!", 100, 100)
+  love.graphics.rectangle("line", x, y, 200, 150)
 end
 
-sayHello()
+
+
