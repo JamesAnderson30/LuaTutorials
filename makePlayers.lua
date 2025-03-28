b78fed180c1e480dbc553bb42d5a00932e044ca3
James Anderson <145408914+JamesAnderson30@users.noreply.github.com>
2025-03-28 14:24:12 -0400

    Update makePlayers.lua

--- makePlayers.lua
+++ makePlayers.lua
@@ -4,7 +4,7 @@ local Bullet = require("Bullet")
 
 function hitBy(hitter)
     if (hitter.cooldown < love.timer.getTime() - hitter.lastHit) then
-      rect.hp = rect.hp - hitter.dmg
+      player.hp = player.hp - hitter.dmg
       hitter.lastHit = love.timer.getTime()
       Collision.handleKnockBack(player, hitter)
     else
@@ -13,31 +13,98 @@ function hitBy(hitter)
   end
 
 
-function shoot(player)
+local function shoot(player)
   --Check Cooldown
+  local bulletX = 0
+  local bulletY = 0
   if(player.cooldown < love.timer.getTime() - player.lastShot) then
-    table.insert(Bullet.bullets, Bullet.makeBasicBullet(player.x + (player.w / 2), player.y + (player.h / 2), "up"))
-    player.lastShot = love.timer.getTime()
+    if player.direction == "up" then
+      bulletX = player.x + (player.w / 2) - (faceW / 2)
+      bulletY = player.y
+    end
+    
+    if player.direction == "down" then
+      bulletX = player.x + (player.w / 2) - (faceW / 2)
+      bulletY = player.y + player.h - FL
+    end
+    
+    if player.direction == "left" then
+      bulletX = player.x
+      bulletY = player.y + (player.h / 2) - (faceH / 2)
+    end
+    
+    if player.direction == "right" then
+      bulletX = player.x + player.w - FL
+      bulletY = player.y + (player.h / 2) - (faceH / 2)
+    end
+    
+      table.insert(Bullet.bullets, Bullet.makeBasicBullet(bulletX, bulletY, player.direction))
+      player.lastShot = love.timer.getTime()
   end
 end
 
+local function draw(player)
+  love.graphics.rectangle(player.kind, player.x, player.y, player.w, player.h)
+  
+  -- "Face Short"
+  FS = 7
+  
+  -- "Face Long"
+  FL = 15
+  
+  love.graphics.setColor(255,0,0) --Red
+  if player.direction == "up" then
+    faceW = FS
+    faceH = FL
+    faceX = player.x + (player.w / 2) - (faceW / 2)
+    faceY = player.y
+  end
+  
+  if player.direction == "down" then
+    faceX = player.x + (player.w / 2) - (faceW / 2)
+    faceY = player.y + player.h - FL
+    faceW = FS
+    faceH = FL
+  end
+  
+  if player.direction == "left" then
+    faceX = player.x
+    faceY = player.y + (player.h / 2) - (faceH / 2)
+    faceW = FL
+    faceH = FS
+  end
+  
+  if player.direction == "right" then
+    faceX = player.x + player.w - FL
+    faceY = player.y + (player.h / 2) - (faceH / 2)
+    faceW = FL
+    faceH = FS
+  end
+  
+  love.graphics.rectangle("line", faceX, faceY, faceW, faceH)
+  
+  love.graphics.setColor(255,255,255) --Red
+end
+
 function playerMaker.makePlayer(x, y, w, h)
-  rect = {}
-  rect.alive = true
-  rect.hp = 100
-  rect.maxHp = 100
-  rect.speed = 125
-  rect.kind = 'fill'
-  rect.x = x
-  rect.y = y
-  rect.w = w
-  rect.h = h
-  rect.weapon = "basic"
-  rect.shoot = shoot
-  rect.lastShot = 0
-  rect.cooldown = 200 / 1000 -- divide by a thousand to measure in milliseconds, my preference
-  rect.hitBy = hitBy
-  return rect
+  player = {}
+  player.alive = true
+  player.hp = 100
+  player.maxHp = 100
+  player.speed = 125
+  player.kind = 'fill'
+  player.direction = "up"
+  player.x = x
+  player.y = y
+  player.w = w
+  player.h = h
+  player.weapon = "basic"
+  player.shoot = shoot
+  player.draw = draw
+  player.lastShot = 0
+  player.cooldown = 200 / 1000 -- divide by a thousand to measure in milliseconds, my preference
+  player.hitBy = hitBy
+  return player
 end
 
 return playerMaker
